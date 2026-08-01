eb3598ff942445ad3178f0e154a666417e4a3fd3
Chibuzo Clifford Okorie <kcalifikz@gmail.com>
2026-08-01 05:48:03 -0700

    Update settings.py

--- healingcircle/settings.py
+++ healingcircle/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,healing-circle-waitlist.vercel.app").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
